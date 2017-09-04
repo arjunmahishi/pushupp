c03e7dded4b73fcd22b27f83bb1c59a136b27314
arunsri7 <arun.i1997@gmail.com>
2017-09-04 15:21:35 +0530

    Added reset button

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ function increment(){
   var count = parseInt(document.querySelector("#count").innerHTML) + 1;
   document.querySelector("#count").innerHTML = count;
 }
+function reset(){
+  document.querySelector("#count").innerHTML = 0;
+}
