56d8becc4da0c99b513e3a0a6ac87501a926fd62
Arjun Mahishi <arjun.mahishi@gmail.com>
2017-09-04 20:18:36 +0530

    Added mobile jquery, and more functionalities

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 function increment(){
-  var count = parseInt(document.querySelector("#count").innerHTML) + 1;
-  document.querySelector("#count").innerHTML = count;
+	var count = parseInt(document.querySelector("#count").innerHTML) + 1;
+	document.querySelector("#count").innerHTML = count;
 }
+
+function decrement(){
+	var count = parseInt(document.querySelector("#count").innerHTML) - 1;
+	document.querySelector("#count").innerHTML = count;
+}
+
 function reset(){
-  document.querySelector("#count").innerHTML = 0;
+	document.querySelector("#count").innerHTML = 0;
+}
+
+$(document).on("pagecreate",function(){
+	$(".main").on("swipeleft",function(){
+		decrement();
+	});           
+});
