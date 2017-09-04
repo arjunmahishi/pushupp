80173f24a24821fad1b3a24e6fead28554084706
Arjun Mahishi <arjun.mahishi@gmail.com>
2017-09-04 21:04:31 +0530

    Added voice

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
+var GOAL = 10;
+
+function speak(text){
+	responsiveVoice.speak(text, "UK English Male");
+}
+
+function showGetNumber(){
+	$("#get-number").show();
+	$("#main").hide();
+}
+
+function showMain(){
+	$("#get-number").hide();
+	$("#main").show();
+}
+
 function increment(){
 	var count = parseInt(document.querySelector("#count").innerHTML) + 1;
 	document.querySelector("#count").innerHTML = count;
+	var goal = parseInt(document.querySelector("#goal").value);
+	var remaining = goal - count;
+	console.log(goal, count, remaining);
+	if(remaining > 0){
+		speak(remaining + " more to go.");
+	}else if(remaining == 0){
+		speak("You have done it!!");
+	}
 }
 
 function decrement(){
@@ -11,6 +35,8 @@ function decrement(){
 function reset(){
 	document.querySelector("#count").innerHTML = 0;
 }
+
+showGetNumber();
 
 $(document).on("pagecreate",function(){
 	$(".main").on("swipeleft",function(){
